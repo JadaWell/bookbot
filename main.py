9736e6d94849f5a50ef98cd92f63c79eee7251bb
JadaWell <sakai.midori.4@gmail.com>
2024-05-30 06:13:01 +0200

    split words

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    words = split_text(text)
+    # print(text)
+    print(words)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def split_text(stg):
+    words = stg.split()
+    return words
+
+
+
+
+
 
 
 main()
